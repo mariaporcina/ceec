95a15e8015c25d3272ba3c2730357513cc931877
Maria Porcina <maria.porcina.mp@gmail.com>
2023-10-03 22:19:59 -0300

    mobile menu: setting up open/close feature; fix: responsiveness

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -7,9 +7,20 @@ import { Component } from '@angular/core';
 })
 
 export class SidebarComponent {
-  isOpen = true;
+  isActive = '';
 
-  handleMenu() {
-    this.isOpen = !this.isOpen;
+  handleMenu($event: MouseEvent) {
+    const button = $event.currentTarget as Element;
+    const icon = button.children[0] as Element;
+
+    if(button.classList.contains('opened')) {
+      this.isActive = '';
+      icon.classList.remove('fa-times');
+      button.classList.remove('opened');
+    } else {
+      this.isActive = 'activeMenu';
+      icon.classList.add('fa-times');
+      button.classList.add('opened');
+    }
   }
 }
