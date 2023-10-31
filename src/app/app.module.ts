6604d62e3ff407faf36ee196839abdea72b52861
Maria Porcina <maria.porcina.mp@gmail.com>
2023-10-30 22:09:39 -0300

    settint up form control; adding length validations; adding regex for cnpj field; repositioning fields

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -36,9 +39,12 @@ import { CompanyFormComponent } from './companies/company-form/company-form.comp
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    NgxMaskDirective,
+    NgxMaskPipe
   ],
-  providers: [],
+  providers: [provideNgxMask()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/companies/company-form/company-form.component.ts
+++ src/app/companies/company-form/company-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-company-form',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./company-form.component.css']
 })
 export class CompanyFormComponent {
+  newCompanyForm = new FormGroup({
+    businessName: new FormControl(''),
+    cnpj: new FormControl(''),
+    name: new FormControl(''),
+    phone: new FormControl(''),
+    cep: new FormControl(''),
+    email: new FormControl(''),
+    status: new FormControl(''),
+  });
 
+  formWarning: string = '';
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    // console.warn(this.newCompanyForm.value);
+  }
+
+  validateInput($event: Event) {
+    const input = $event.target as Element;
+    if((input as HTMLInputElement).validity.valid) {
+      this.formWarning = "";
+    } else {
+      this.formWarning = "Digite um CNPJ válido.";
+    }
+  }
 }
