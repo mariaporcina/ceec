75e2fcfec45f25614d23ee2888731fd8402be568
Maria Porcina <43049787+mariaporcina@users.noreply.github.com>
2023-11-28 19:39:57 -0300

    Revert "Fix/Company form"

--- src/app/model/company.ts
+++ src/app/model/company.ts
@@ -1,23 +1,21 @@
 export class Company {
     id?: string;
-    businessName: string;
+    corporateName: string;
     name: string;
     cnpj: string;
     email: string;
-    phone: string;
-    cep: string;
+    postalCode: string;
     city: string;
     state: string;
     status?: boolean;
 
-    constructor(businessName: string, name: string, cnpj: string, email: string, phone: string, cep: string, city: string, state: string, status: boolean = true) {
+    constructor(corporateName: string, name: string, cnpj: string, email: string, postalCode: string, city: string, state: string, status: boolean = true) {
         this.id = String(Math.round(Math.random() * 1000));
-        this.businessName = businessName;
+        this.corporateName = corporateName;
         this.name = name;
         this.cnpj = cnpj;
         this.email = email;
-        this.phone = phone;
-        this.cep = cep;
+        this.postalCode = postalCode;
         this.city = city;
         this.state = state;
         this.status = status;
